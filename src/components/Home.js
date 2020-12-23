54bef7c7c4e034f1a1082cf83efd08674027f8f0
gaia77 <J@kass77>
2020-12-23 15:54:47 -0800

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 // Home
 
+import poster from '../images/diehard.jpg';
+
+
 function Home() {
     return (
       <main>
         <section>
             <h2>Home Page</h2>
         </section>
+        <picture>
+          <img src={poster} alt="Die hard movie poster"></img>
+        </picture>
       </main>
     );
   }
