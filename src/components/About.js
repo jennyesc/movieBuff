1f0670ccd9333d6084d6737944810b510ef9a433
gaia77 <J@kass77>
2021-01-07 22:48:26 -0800

    About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,37 @@
 //About
+import background from '../images/about-bg.jpg';
 
 function About () {
     return (
         <main>
             <section>
-                <div className="page-info">
-                    <h1>Hello!</h1>
-                </div> 
-                <div className="about-text">   
-                    <p>Welcome to The Movie Buff movie and TV database world. We are a 
-                        community of Movie buffs, who love to share their opinion on 
-                        movies and TV shows. We pride ourselves in running one of the
-                        most diverse movie and TV database to date.
-                    </p>
-                    <p>The Movie Buff started in 2014 when there was a need for a people
-                        movie and TV database community. Word spread and here we are with 
-                        over 10,000 memebers and growing every day.
-                    </p>
-                    <h3>How it works</h3>
-                    <ol>
-                        <li>Join the community by craeting an account.</li>
-                        <li>Customize your profile and read the Community Guidelines.</li>
-                    </ol>
+                <div className="page-info-container">
+                    <div className="page-bg">
+                        <img src={background} alt="About page backgroung" ></img>
+                        <div className="about-text">
+                            <h1>Hello!</h1>
+                                
+                            <p>Welcome to The Movie Buff movie and TV database world. We are a 
+                                community of Movie buffs, who love to share their opinion on movies and TV shows. We pride ourselves in running one of the
+                                most diverse movie and TV database to date.
+                            </p>
+                            <p>The Movie Buff started in 2014 when there was a need for a people
+                                    movie and TV database community. Word spread and here we are with 
+                                    over 10,000 memebers and growing every day.
+                            </p>
+                                <h3>How it works</h3>
+                            <div className="btn-container">
+                                <p className="circle"> 1 </p> 
+                                <button className="btn"> Join the community by creating an account.</button>
+                                <p className="circle"> 2 </p> 
+                                <button className="btn">Customize your profile and read the Community Guidelines.</button>
+                            </div> 
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
+       
     );
 }
 
