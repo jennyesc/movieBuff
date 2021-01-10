a2622fe8401218a082d2c085e6c74c7bc42f5804
gaia77 <J@kass77>
2021-01-10 14:28:41 -0800

    about page updates

--- src/components/About.js
+++ src/components/About.js
@@ -29,15 +29,30 @@ function About () {
                                     <div className="btn-one">
                                         <p className="circle">1</p> 
                                         <button className="btn"> 
-                                            <p>Join the community by</p>
-                                            <p className="color">creating an account.</p>
+                                            <p>Join the community by
+                                            <span className="color"> creating an account.</span></p>
                                         </button>
                                     </div>
                                     <div className="btn-two">
                                         <p className="circle">2</p> 
                                         <button className="btn">
-                                            <p>Customize your profile and read the</p> 
-                                            <p className="color">Community Guidelines.</p>
+                                            <p>Customize your profile and read the 
+                                            <span className="color">  Community Guidelines. </span></p>
+                                        </button>
+                                    </div>
+                                    <div className="btn-three">
+                                        <p className="circle">3</p> 
+                                        <button className="btn"> 
+                                            <p>Browse for movies and TV shows. When you find one you like,
+                                                you can rate them, add to your favourites and leave a review.</p>
+                                        </button>
+                                    </div>
+                                    <div className="btn-four">
+                                        <p className="circle">4</p> 
+                                        <button className="btn"> 
+                                            <p>Join the
+                                            <span className="color"> forums </span>
+                                            and talk to other Movie Buffs about the latest</p>
                                         </button>
                                     </div>
 
