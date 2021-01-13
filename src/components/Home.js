012d20cb60fadd8f17b46dc55251336e50e83ff4
jennyesc <71670394+jennyesc@users.noreply.github.com>
2021-01-12 23:49:46 -0800

    home page desktop view

--- src/components/Home.js
+++ src/components/Home.js
@@ -65,6 +65,111 @@ function Home() {
           </div>
         
         </section>
+
+        <section className="movie-container">
+          
+          
+          <div className="movie-poster">
+              <img className="poster-image" src={poster} alt="Die hard movie poster" ></img>
+            
+              <div className="movie-rating">
+                <p> 44%</p>
+              </div>
+          </div>
+          
+          <div className="movie-description">
+              <h2>Hard Kill</h2>
+              <h3>Oct 23, 2020</h3>
+              <p>The work of billionaire tech CEO Donavan Chalmers is so valuable
+               that he hires ...</p>
+          </div>
+        
+        </section>
+
+
+        <section className="movie-container">
+          
+          
+          <div className="movie-poster">
+              <img className="poster-image" src={poster} alt="Die hard movie poster" ></img>
+            
+              <div className="movie-rating">
+                <p> 44%</p>
+              </div>
+          </div>
+          
+          <div className="movie-description">
+              <h2>Hard Kill</h2>
+              <h3>Oct 23, 2020</h3>
+              <p>The work of billionaire tech CEO Donavan Chalmers is so valuable
+               that he hires ...</p>
+          </div>
+        
+        </section>
+
+        <section className="movie-container">
+          
+          
+          <div className="movie-poster">
+              <img className="poster-image" src={poster} alt="Die hard movie poster" ></img>
+            
+              <div className="movie-rating">
+                <p> 44%</p>
+              </div>
+          </div>
+          
+          <div className="movie-description">
+              <h2>Hard Kill</h2>
+              <h3>Oct 23, 2020</h3>
+              <p>The work of billionaire tech CEO Donavan Chalmers is so valuable
+               that he hires ...</p>
+          </div>
+        
+        </section>
+
+        <section className="movie-container">
+          
+          
+          <div className="movie-poster">
+              <img className="poster-image" src={poster} alt="Die hard movie poster" ></img>
+            
+              <div className="movie-rating">
+                <p> 44%</p>
+              </div>
+          </div>
+          
+          <div className="movie-description">
+              <h2>Hard Kill</h2>
+              <h3>Oct 23, 2020</h3>
+              <p>The work of billionaire tech CEO Donavan Chalmers is so valuable
+               that he hires ...</p>
+          </div>
+        
+        </section>
+
+
+        <section className="movie-container">
+          
+          
+          <div className="movie-poster">
+              <img className="poster-image" src={poster} alt="Die hard movie poster" ></img>
+            
+              <div className="movie-rating">
+                <p> 44%</p>
+              </div>
+          </div>
+          
+          <div className="movie-description">
+              <h2>Hard Kill</h2>
+              <h3>Oct 23, 2020</h3>
+              <p>The work of billionaire tech CEO Donavan Chalmers is so valuable
+               that he hires ...</p>
+          </div>
+        
+        </section>
+
+
+        
       </main>
     );
   }
