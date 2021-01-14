434576864c749125732214651832fa495d2c49d3
gaia77 <J@kass77>
2021-01-13 20:16:55 -0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import poster from '../images/diehard.jpg';
 
 function Home() {
     return (
-      <main>
+      <main className="grid">
        <section className="movie-container">
           
           
