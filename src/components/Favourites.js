f97f05095edfb5630358cd2e0af9b532fbb6291b
jennyesc <71670394+jennyesc@users.noreply.github.com>
2021-01-15 00:01:54 -0800

    favourites

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -9,7 +9,7 @@ function Favourites() {
     return (
       <main>
         <div className="line"> </div>
-        <section className="fav-movie-cont">
+        <section className="fav-movie-heading">
             <h2>My Favourites</h2>
             <ul>
               <li>Movies (3)</li>
@@ -18,6 +18,7 @@ function Favourites() {
             </ul>
         </section>
 
+      <div className="desktop-fav-container">
         <section className="fav-movie-container">
     
           <div className="movie-poster">
@@ -28,10 +29,11 @@ function Favourites() {
               </div>
           </div>
           
-          <div className="movie-description">
-            <div className="">
-            <p className="circle">46%</p>
+            <div className="circle-container">
+                <p className="circle">46%</p>
             </div>
+          <div className="movie-description">
+          
               <h2>Hard Kill</h2>
               <h3>Oct 23, 2020</h3>
               <p>The work of billionaire tech CEO Donavan Chalmers is so valuable
@@ -73,7 +75,73 @@ function Favourites() {
           </div>
 
         </div>
+      </div>
+      
+      <div className="desktop-fav-container">
+        <section className="fav-movie-container">
+    
+          <div className="movie-poster">
+              <img className="poster-image" src={poster} alt="Die hard movie poster" ></img>
+            
+              <div className="movie-rating">
+                <p> 44%</p>
+              </div>
+          </div>
+          
+          <div className="circle-container">
+              <p className="circle">46%</p>
+          </div>
 
+
+          <div className="movie-description">
+          
+
+            
+            
+              <h2>Hard Kill</h2>
+              <h3>Oct 23, 2020</h3>
+              <p>The work of billionaire tech CEO Donavan Chalmers is so valuable
+               that he hires ...</p>
+          </div>
+        </section>
+
+        <div className="play-trailer-bar">
+            <div className="play-icon">  
+                <i className="fa"> <FontAwesomeIcon icon="play-circle" /> </i>
+                <p>Play Trailer</p>
+                
+            </div>   
+        </div>
+
+        <div className="fav-input-container">
+          <div className="icon-container">
+            <div className="icon rating-icon">  
+                  <i className="fa"> <FontAwesomeIcon icon="star" /> </i>
+                  <p>Your Rating</p>
+            </div> 
+
+            <div className=" icon fav-icon">  
+                  <i className="fa"> <FontAwesomeIcon icon="heart" /> </i>
+                  <p>Favourite</p>
+            </div> 
+            <div className="icon list-icon">  
+                  <i className="fa"> <FontAwesomeIcon icon="list" /> </i>
+                  <p>Add to List</p>
+            </div> 
+
+            <div className="icon remove-icon">  
+                  <i className="fa"> <FontAwesomeIcon icon="times-circle" /> </i>
+                  <p>Remove</p>
+            </div> 
+
+
+
+          </div>
+
+        </div>
+      </div>
+      
+      
       </main>
     );
   }
