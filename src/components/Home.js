e2098799103bcd886b50ac4f0f55dc130b8d0cd5
gaia77 <J@kass77>
2021-01-16 22:43:42 -0800

    Movie selection form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,42 @@
 // Home
+
+import { useState, useEffect } from 'react';
+import { MD_BASE_URL, MD_API_KEY } from '../globals/variables';
 import { Link } from 'react-router-dom';
 import poster from '../images/diehard.jpg';
 
 
-function Home() {
+const Home = () => {
+
+      const fetchMovie = async () => {
+          const res = await fetch( `${MD_BASE_URL}${MD_API_KEY}`);
+          let data = await res.json();
+          console.log(data);
+      }
+
+      fetchMovie();
+
+
+  useEffect( () => {
+
+  }, [] );
+
+
+//function Home() {
     return (
       <main>
+        <div className="line"> </div>
+          <form className="form-select-group">
+            <div className="input-group input-select-group">
+              {/* <label htmlFor="">Movies</label> */}
+                <select id="selectChart" name="selectChart" className="selection">
+                  <option value="popular" selected>Popular</option>
+                  <option value="top_rated">Top Rated</option>
+                  <option value="now_playing">Now Playing</option>
+                  <option value="upcoming">Upcoming</option>
+                </select>  
+            </div>  
+          </form>
         <div className="desktop-grid">
        <section className="index-movie-container">
           
@@ -22,7 +53,7 @@ function Home() {
               <h2>Hard Kill</h2>
               <div className="dt-movie-infobox">
                 <h3>Oct 23, 2020</h3>
-                <p className="dt-more-infolink"> More Info </p>
+                <Link to="/singleMovie"><p className="dt-more-infolink"> More Info </p></Link>
                 {/* <Link to="/singleMovie">About</Link> */}
               
                 <p>The work of billionaire tech CEO Donavan Chalmers is so valuable
