76deec12f1ac8eb18727f8c1ddbcdb05df4588c6
gaia77 <J@kass77>
2021-01-24 09:06:17 -0800

    movie-sort-nav styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -64,7 +64,9 @@ const Home = () => {
           <ul>
             <li><NavLink to={'/sort/popular'}>Popular</NavLink></li>
             <li><NavLink to={'/sort/top-rated'}>Top Rated</NavLink></li>
+            <li><NavLink to={'/sort/now-plating'}>Now Playing</NavLink></li>
             <li><NavLink to={'/sort/upcoming'}>Upcoming</NavLink></li>
+
           </ul>
         </nav>
           
