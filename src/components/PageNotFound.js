9340d0c56316039c8729e6f11cd0db75c61aa757
gaia77 <J@kass77>
2021-01-25 09:16:47 -0800

    404 Page not found styling

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,23 +5,23 @@ import background from '../images/about-bg.jpg';
 
 function PageNotFound () {
 	return (
-		// <main>
-		// 	<section>
+		 <main>
+			<section>
 				<div className="page-container">
 						<div className="page-background">
 							<div className="image" style={{ backgroundImage: `url(${background})`}}>
 								<div className="page-container">
 									<h2 className="align">404...Page Not Found</h2>
 									<div className="p">
-										<p>Go to the <Link to={'/'}>home</Link> page.</p>
+										<p>Please go to the <Link to={'/'}>home</Link> page.</p>
 									</div>
 								</div>	
 							</div>
 						</div>	
 				</div>
 				
-		// 	</section>
-		// </main>
+		 	</section>
+		 </main>
 
 	);
 }
